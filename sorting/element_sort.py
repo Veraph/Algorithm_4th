5aab11c8f7c9101a6a8219185ad25a742fe71867
Veraph <jmw3531@live.com>
2020-08-09 08:20:24 +0100

    update code.

--- sorting/element_sort.py
+++ sorting/element_sort.py
@@ -3,6 +3,11 @@ import time
 array = [7, 3, 2, 5, 6, 8, 0, 1]
 len_array = len(array)
 
+def exch(array, i, j):
+    temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+
 # Selection sort
 array1 = array[:]
 start_time = time.time()
@@ -25,12 +30,11 @@ print(array1)
 array1 = array[:]
 start_time = time.time()
 
-for i in range(1, len_array):
-        for j in range(i, 0, -1):
-            if array1[j] < array1[j-1]:
-                temp = array1[j-1]
-                array1[j-1] = array1[j]
-                array1[j] = temp
+for i in range(1, len_array-1):
+    j = i
+    while (j > 0 and array1[j] < array1[j-1]):
+        exch(array1, j, j-1)
+        j -= 1
 
 time_consumped = time.time() - start_time              
 print('insertion sort: %s' % time_consumped)
@@ -42,14 +46,13 @@ start_time = time.time()
 
 h = 1
 while h < len_array / 3:
-    h = 3*h + 1
+    h = h*3 + 1
 while h >= 1:
-    for i in range(h, len_array):
-        for j in range(i, h-1, -h):
-            if array1[j] < array1[j-h]:
-                temp = array1[j-h]
-                array1[j-h] = array1[j]
-                array1[j] = temp
+    for i in range(h, len_array-1):
+        j = i
+        while (j >= h and array1[j] < array1[j-h]):
+            exch(array1, j, j-h)
+            j -= h
     h = int(h/3)
 
 time_consumped = time.time() - start_time              
